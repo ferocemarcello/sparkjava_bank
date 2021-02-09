795ef0df65bea82358898b169a8369574b045535
Marcello Feroce <ferocemarcello@gmail.com>
2021-02-09 18:18:55 +0100

    working on unit tests

--- src/test/java/BankTest.java
+++ src/test/java/BankTest.java
@@ -1,22 +1,12 @@
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.bankbridge.Main;
 import io.bankbridge.model.BankDao;
-import junit.framework.TestCase;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 import java.io.IOException;
 import java.net.URI;
@@ -25,8 +15,6 @@ import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
@@ -47,32 +35,75 @@ public class BankTest{
     @Test
     public void testBankVersionOne() throws IOException, InterruptedException, ParseException {
         String url = "http://localhost:8080/v1/banks/all";
+
+        HttpResponse response = geResponse(url);
+        int statusCode = response.statusCode();
+        //String body = response.body(); we might need the bod
+
+        HttpHeaders headers = response.headers();
+        String version = headers.allValues("version").get(0);
+        String num_banks_header = headers.allValues("num_banks").get(0);
+
+        Map<String,Object> html_return = getHtmlContent(url);
+        Set<Set<Object>> bankset_json = getBankSet_vone();
+        Set<Set<Object>> bankset_html = (Set<Set<Object>>) html_return.get("bankset");
+
+        assertEquals(statusCode, 200);
+        assertEquals(version, "1");
+        assertEquals(num_banks_header, Integer.toString((Integer) html_return.get("num_banks")),"20");
+        assertTrue(bankset_html.equals(bankset_json));
+    }
+
+    @Test
+    public void testBankVersionTwo() throws IOException, InterruptedException, ParseException {
+        String url = "http://localhost:8080/v2/banks/all";
+
+        HttpResponse response = geResponse(url);
+        int statusCode = response.statusCode();
+        //String body = response.body(); we might need the bod
+
+        HttpHeaders headers = response.headers();
+        String version = headers.allValues("version").get(0);
+        String num_banks_header = headers.allValues("num_banks").get(0);
+
+        Map<String,Object> html_return = getHtmlContent(url);
+        Set<Set<Object>> bankset_json = getBankSet_vone();
+        Set<Set<Object>> bankset_html = (Set<Set<Object>>) html_return.get("bankset");
+
+        assertEquals(statusCode, 200);
+        assertEquals(version, "2");
+        assertEquals(num_banks_header, Integer.toString((Integer) html_return.get("num_banks")),"20");
+        assertTrue(bankset_html.equals(bankset_json));
+    }
+
+    private HttpResponse<String> geResponse(String url) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest req = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
         HttpResponse.BodyHandler<String> bh = HttpResponse.BodyHandlers.ofString();
         HttpResponse<String> response = client.send(req, bh);
-        int statusCode = response.statusCode();
-        //String body = response.body(); we might need the bod
-        Map<String,Object> html_return = getHtmlContent(url);
-        HttpHeaders headers = response.headers();
-        String version = headers.allValues("version").get(0);
-        String num_banks_header = headers.allValues("num_banks").get(0);
-        var db_banks = Main.bankDao.filterBanks(new String[]{"name","bic","countryCode","products"});
-        Set<Object> bankset = new HashSet<>();
-        db_banks.forEach(x-> bankset.add((HashSet<Object>)x.values()));
-        headers.allValues("date");
-        assertEquals(statusCode, 200);
-        assertEquals(version, "1");
-        assertEquals(num_banks_header, Integer.toString((Integer) html_return.get("num_banks")),"20");
+
+        return response;
+    }
+
+    private Set<Set<Object>> getBankSet_vone() {
+        var json_banks = Main.bankDao.filterBanks(new String[]{"name","bic","countryCode","products"});
+        Set<Set<Object>> bankset = new HashSet<>();
+
+        for (var m: json_banks) {
+            HashSet<Object> ob = new HashSet<>();
+            m.values().forEach(val -> ob.add(val));
+            bankset.add(ob);
+        }
+        return bankset;
     }
 
     private Map<String, Object> getHtmlContent(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
         int num_banks_html = (int)doc.select("ul[id$=banklist]").get(0).childNodes().stream().filter(n -> n.getClass().getName() ==  "org.jsoup.nodes.Element").count();
         var result = doc.select("ul[id$=banklist]").get(0).childNodes().stream().filter(n -> n.getClass().getName() ==  "org.jsoup.nodes.Element").collect(Collectors.toList());
-        Set<Set<Object>> banklist = new HashSet<>();
+        Set<Set<Object>> bankset = new HashSet<>();
         for (Node n: result
         ) {
             List<Node> collect = n.childNodes().get(1).childNodes().stream().filter(no -> no.getClass().getName() == "org.jsoup.nodes.Element").collect(Collectors.toList());
@@ -87,11 +118,11 @@ public class BankTest{
                 }
                 else {values.add(element);}
             }
-            banklist.add(values);
+            bankset.add(values);
         }
         Map<String, Object> html_content = new HashMap<>();
         html_content.put("num_banks",num_banks_html);
-        html_content.put("banklist",banklist);
+        html_content.put("bankset",bankset);
         return html_content;
     }
 }
